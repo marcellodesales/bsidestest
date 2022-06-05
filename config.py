48a0ebbaa2b605e9ee530d5f7adf5843582a7e68
Mackenzie <mackenziejackson@live.com>
2022-06-05 07:44:14 -0700

    updating config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 [default]
-aws_access_key_id = AKIAYVP4CIPPBBAUNRBG
-aws_secret_access_key = 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
+aws_access_key_id = AKIAYVP4CIPPLD7GPBON
+aws_secret_access_key = 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
 output = json
 region = us-east-2
