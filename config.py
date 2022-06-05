255c2699ff084df7e5eaed22a1bade66fedafdfb
Mackenzie <mackenziejackson@live.com>
2022-06-05 08:33:08 -0700

    Updating the config file with cloud access

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 [default]
-aws_access_key_id = AKIAYVP4CIPPLD7GPBON
-aws_secret_access_key = 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
+aws_access_key_id = AKIAYVP4CIPPKRTUG5VB
+aws_secret_access_key = 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
 output = json
 region = us-east-2
