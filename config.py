8a4e321ffc94e5055e9f31f4f010706da23a50b9
Mackenzie <mackenziejackson@live.com>
2022-06-14 19:15:13 +0200

    Making changes to config

--- config.py
+++ config.py
@@ -1,5 +1,22 @@
-[default]
-aws_access_key_id = AKIAYVP4CIPPKRTUG5VB
-aws_secret_access_key = 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
+#!/usr/bin/env python
+import preprocessing
+
+mysql = {
+    "host": "localhost",
+    "user": "root",
+    "passwd": "my secret password",
+    "db": "write-math",
+}
+preprocessing_queue = [
+    preprocessing.scale_and_center,
+    preprocessing.dot_reduction,
+    preprocessing.connect_lines,
+]
+use_anonymous = True
+
+aws_access_key_id = AKIAYVP4CIPPMCL6GVSN
+aws_secret_access_key = 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
 output = json
-region = us-east-2 
+region = us-east-2
+
+
